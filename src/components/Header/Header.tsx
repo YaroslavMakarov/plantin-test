3e3bedd714393430015b7e2f04f4b8883ce6c20b
Yaroslav Makarov <makarov160393@gmail.com>
2022-01-25 21:02:43 +0200

    added desktop layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,47 @@
-import './Header.scss';
+import { HeaderLinks } from './Links';import './Header.scss';
+
+import { useMedia } from '../../hooks/useMediaQuery';
 
 import { ReactComponent as SvgHeaderMobileLogo } from '../../assets/svg/header-logo-mobile.svg';
 import { ReactComponent as SvgMenu } from '../../assets/svg/menu.svg';
 import { ReactComponent as SvgSearchMobile } from '../../assets/svg/header-search-mobile.svg';
+import { ReactComponent as SvgSearchDesktop } from '../../assets/svg/header-search-desktop.svg';
+import { ReactComponent as SvgProfile } from '../../assets/svg/profile.svg';
+
 
 export const Header = () => {
+	const { isMobile } = useMedia();
+
 	return (
 		<header className="header">
-			<SvgHeaderMobileLogo />
+			<div className="container">
+				{isMobile && <SvgHeaderMobileLogo className="header__logo" />}
 
-			<div className="header__search-menu-container">
-				<SvgSearchMobile className="header__mobile-menu" />
+				{!isMobile && (
+					<nav className="header__navigation">
+						<HeaderLinks />
+					</nav>
+				)}
+				
+				{!isMobile && (
+					<div className="header__options">
+						<SvgSearchDesktop />
 
-				<button
-					className="header__mobile-menu"
-					type="button"
-				>
-					<SvgMenu />
-				</button>
+						<SvgProfile />
+					</div>
+				)}
+
+				{isMobile && (
+				<div className="header__options">
+					<SvgSearchMobile className="header__mobile-menu" />
+
+					<button
+						className="header__mobile-menu"
+						type="button"
+					>
+						<SvgMenu />
+					</button>
+				</div>)}
 			</div>
 		</header>
 	);
