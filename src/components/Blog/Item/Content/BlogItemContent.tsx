6142821a97f4547c44db842820c3e07eec3030a6
Yaroslav Makarov <makarov160393@gmail.com>
2022-01-27 12:41:07 +0200

    created  item content component

---
+++ src/components/Blog/Item/Content/BlogItemContent.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import moment from 'moment';
+
+type Props = {
+	title: string,
+	content: string,
+	readingTime: number,
+	createdAt: string,
+};
+
+import './BlogItemContent.scss';
+
+export const BlogItemContent: FC<Props> = (
+	{ title, content, readingTime, createdAt }
+) => {
+	return (
+		<div className="blog-item-content">
+			<div className="blog-item-content__date">
+				{`
+					${moment(createdAt, 'x').format('MMM DD')}
+						· ${readingTime} min read
+				`}
+			</div>
+
+			<h3 className="blog-item-content__header">
+				{title}
+			</h3>
+
+			<p className="blog-item-content__text">
+				{content}
+			</p>
+		</div>
+	);
+};
