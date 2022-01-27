811a933699dac33d8f4648227880d137bb6f18b0
Yaroslav Makarov <makarov160393@gmail.com>
2022-01-27 12:58:34 +0200

    created blog top component

--- src/components/Blog/Item/BlogItem.tsx
+++ src/components/Blog/Item/BlogItem.tsx
@@ -5,7 +5,7 @@ import { Image } from '../../Image';
 
 import './BlogItem.scss';
 
-export const BlogItem: FC<Article> = ({ article }) => {
+export const BlogItem: FC<ArticleProps> = ({ article }) => {
 	const { img, title, content, readingTime, createdAt } = article;
 
 	return (
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+type Article = {
+	id: number,
+	img: string,
+	title: string,
+	content: string,
+	readingTime: number,
+	createdAt: string,
+}
+
+type ArticleProps = {
+	article: Article
+}
+
+type ArticlesProps = {
+	articles: Article[]
+}
